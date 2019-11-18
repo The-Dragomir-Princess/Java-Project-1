7a13362a9382e4c5a47f4a0c66cea9975649544d
The-Dragomir-Princess <43792821+The-Dragomir-Princess@users.noreply.github.com>
2019-11-18 17:49:04 +0530

    Update project.java

--- project.java
+++ project.java
@@ -10,7 +10,9 @@ class banking
 	int age;
  	String name, address;
 	static int c=1000;
-	FileWriter f=new FileWriter(fileName:"/home/twisha/Desktop/Acount_details.txt")
+	int x=0;
+	FileWriter f=new FileWriter(fileName:"/home/twisha/Desktop/Acount_details.txt");
+	BufferedWriter f1 = new BufferedWriter(f);
 	public void Create_account()
 	{
 		Scanner sc= new Scanner(System.in);
@@ -22,5 +24,14 @@ class banking
 		age=sc.nextInt();
 		System.out.println("Enter your address:");
 		address=sc.nextLine();
-		
-		
+		c++;
+		x++;
+		f1.write("Customer"+ x + "Details:");
+		f1.newLine();
+		f1.write(name);
+		f1.newLine();
+		f1.write(age);
+		f1.newLine();
+		f1.write(address);
+		f1.newLine();
+	}
