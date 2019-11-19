80e2c42db87f09fe79d2290f5fc15558b4d83630
The-Dragomir-Princess <43792821+The-Dragomir-Princess@users.noreply.github.com>
2019-11-19 09:43:06 +0530

    Update project.java

--- project.java
+++ project.java
@@ -7,31 +7,45 @@ import java.io.BufferedReader;
 
 class banking
 {	
-	int age;
- 	String name, address;
-	static int c=1000;
-	int x=0;
-	FileWriter f=new FileWriter(fileName:"/home/twisha/Desktop/Acount_details.txt");
-	BufferedWriter f1 = new BufferedWriter(f);
-	public void Create_account()
-	{
-		Scanner sc= new Scanner(System.in);
-		System.out.println("Enter your name:");
-		name=sc.nextLine();
-		System.out.println("Enter your age:");
-		age=sc.nextInt();
-		System.out.println("Enter your age:");
-		age=sc.nextInt();
-		System.out.println("Enter your address:");
-		address=sc.nextLine();
-		c++;
-		x++;
-		f1.write("Customer"+ x + "Details:");
-		f1.newLine();
-		f1.write(name);
-		f1.newLine();
-		f1.write(age);
-		f1.newLine();
-		f1.write(address);
-		f1.newLine();
-	}
+		String age;
+	 	String name;
+	 	String a;
+		int c=1000;
+		
+			
+		
+		public void Create_Account()	throws IOException
+		{
+		
+		FileWriter f=new FileWriter("/home/twisha/Desktop/Project/Acount_details.txt");
+		BufferedWriter f1 = new BufferedWriter(f);
+		
+			Scanner sc= new Scanner(System.in);
+			System.out.println("Enter your name:");
+			name=sc.nextLine();
+			System.out.println("Enter your age:");
+			age=sc.nextLine();
+			System.out.println("Enter your address:");
+			a=sc.nextLine();
+			c++;
+			
+			f1.write("Customer Details:");
+			f1.write("  ");
+			f1.write(" ");
+			f1.newLine();
+			f1.write("Account" + c);
+			f1.newLine();
+			f1.write(name);
+			f1.newLine();
+			f1.write(age);
+			f1.newLine();
+			f1.write(a);
+			
+			f1.newLine();
+			f1.close();
+			f.close();
+		
+		}
+	
+	
+	
